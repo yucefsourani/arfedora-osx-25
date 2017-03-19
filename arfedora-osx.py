02e94c84f8d4e8fb3f13d740072bc74f9d1b7603
yucef sourani <yucefsourani@users.noreply.github.com>
2017-03-19 10:59:34 +0200

    Update arfedora-osx.py

--- arfedora-osx.py
+++ arfedora-osx.py
@@ -3,7 +3,7 @@
 #
 #  arfedora-osx.py
 #  
-#  Copyright 2017 youcefsourani <youcef.m.sourani@gmail.com>
+#  Copyright 2017 youcefsourani <youssef.m.sourani@gmail.com>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
